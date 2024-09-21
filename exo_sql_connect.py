910292c7cfc8752f5bccd9e423fcd87f0bb1125c
ThomasCoutarel <thomascout@hotmail.com>
2024-09-21 19:36:32 +0200

    commit pipeline exo

--- exo_sql_connect.py
+++ exo_sql_connect.py
@@ -1,10 +1,3 @@
-
-
-# cur = db.cursor()
-# cur.execute("SELECT * FROM enseignants;")
-# resultat = cur.fetchall()
-# for i in resultat:
-#     print(i)
 from  mysql.connector import connect, Error
 db= connect(
     host = "localhost",
@@ -13,41 +6,65 @@ db= connect(
     database = "ecole"
 )
 
-def fetch_data(connection):
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM etudiants;")
-        etudiants = cursor.fetchall()
-        cursor.execute("SELECT * FROM enseignants;")
-        enseignants = cursor.fetchall()
-        return etudiants, enseignants
+def print_values(rows):
+    for row in rows:
+        print(row)
 
-connection = db
+def select_values(table):
+    cursor = db.cursor()
+    cursor.execute(f" SELECT * FROM {table}")
+    rows = cursor.fetchall()
+    cursor.close()
+    return rows
 
-etudiants, enseignants = fetch_data(db)
 
-for i in fetch_data(db):
-    i=list(i)
-    print(i)
+"""
+print_values(select_values('enseignants'))
+(1, 'Jonas', 'Salk', None, 'Biologie', None, 'jsalk@school.org', '777-555-4321', '5')
+(3, 'Élise', 'Laurent', 'B202', 'Physique', datetime.date(2023, 5, 15), 'elise.laurent@example.com', '0723456789', 'CL2')
+(4, 'Victor', 'Carpentier', 'B203', 'Chimie', datetime.date(2023, 5, 15), 'victor.carpentier@example.com', '0734567890', 'CL1')
+"""
 
-association = [
-    (etudiant, enseignant)
-    for etudiant in etudiants
-    for enseignant in enseignants
-    if etudiant[7] == enseignant[8]
-]
+def associations():
+    etudiants = select_values('etudiants')
+    enseignants = select_values('enseignants')
 
-print(association)
+    enseignant_by_class = {}
+    for enseignant in enseignants:
+        enseignant_by_class[enseignant[8]] = (enseignant[1], enseignant[2])
 
-eleves_par_enseignant = {}
+    associations = {}
+    for etudiant in etudiants:
+        if etudiant[7]  in enseignant_by_class:
+            enseignant_prenom, enseignant_nom = enseignant_by_class[etudiant[7] ]
+            enseignant_key = (enseignant_prenom, enseignant_nom)
+            if enseignant_key not in associations:
+                associations[enseignant_key] = []
+            associations[enseignant_key].append((etudiant[1], etudiant[2], etudiant[7] ))
+    return associations
 
-for etudiant, enseignant in association:
-    enseignant_nom = enseignant[1]
-    if enseignant_nom in eleves_par_enseignant:
-        eleves_par_enseignant[enseignant_nom] += 1
-    else:
-        eleves_par_enseignant[enseignant_nom] = 1
+"""
+associations=associations()
+print(associations)
+{('Jonas', 'Salk'): [('Mark', 'Watney', '5')], 
+('Victor', 'Carpentier'): [('Alice', 'Dupont', 'CL1'), ('Claire', 'Durand', 'CL1'), ('Emma', 'Bernard', 'CL1'), ('Gabrielle', 'Simon', 'CL1'), ('Isabelle', 'Boucher', 'CL1'), ('Karine', 'Fournier', 'CL1'), ('Marie', 'Lecoq', 'CL1'), ('Océane', 'Garnier', 'CL1'), ('Quentin', 'Pichon', 'CL1'), ('Sophie', 'Fischer', 'CL1')], 
+('Élise', 'Laurent'): [('Benoit', 'Martin', 'CL2'), ('David', 'Leroy', 'CL2'), ('François', 'Moreau', 'CL2'), ('Hugo', 'Lemoine', 'CL2'), ('Jules', 'Giraud', 'CL2'), ('Louis', 'Vasseur', 'CL2'), ('Nicolas', 'Delaunay', 'CL2'), ('Pierre', 'Noir', 'CL2'), ('Romain', 'Collet', 'CL2')]}
+"""
 
-for enseignant, nombre_eleves in eleves_par_enseignant.items():
-    print(f"Enseignant: {enseignant} a {nombre_eleves} élèves.")
+def nb_nombre_etudiants_prof(associations):
+    nb_nombre_etudiants_prof = {}
+    for enseignant_key, etudiants in associations.items():
+        if enseignant_key in nb_nombre_etudiants_prof:
+            nb_nombre_etudiants_prof[enseignant_key] += len(etudiants)
+        else:
+            nb_nombre_etudiants_prof[enseignant_key] = len(etudiants)
+    return nb_nombre_etudiants_prof
 
-db.close()
+
+"""
+associations=associations()
+print(nb_nombre_etudiants_prof(associations))
+{('Jonas', 'Salk'): 1, 
+('Victor', 'Carpentier'): 10, 
+('Élise', 'Laurent'): 9}
+"""
